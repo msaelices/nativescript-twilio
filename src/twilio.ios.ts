2b556b0469655d9b04c51fe8fc7cf0f7caf48149
Manuel Saelices <msaelices@gmail.com>
2019-01-22 14:29:24 +0100

    First iOS implementation

--- src/twilio.ios.ts
+++ src/twilio.ios.ts
@@ -1,8 +1,48 @@
-import { Common } from './twilio.common';
+import * as common from './twilio.common';
 
-export class Twilio extends Common {
+declare var NSDictionary: any;
+declare var NSError: any;
+declare var TVOCallDelegate: any;
+declare var TwilioVoice: any;
+
+export const getAccessToken = common.getAccessToken;
+
+
+class CallDelegate extends NSObject implements TVOCallDelegate {
+  static ObjCProtocols = [TVOCallDelegate];
+  listener: any;
+
+  public static initWithListener(listener: any): CallDelegate {
+    let callDelegate = new CallDelegate();
+    callDelegate.listener = listener;
+
+    return callDelegate;
+  }
+
+  callDidConnect(call: TVOCall) {
+    console.log('callDidConnect');
+    this.listener.onConnected(call);
+  }
+
+  callDidDisconnectWithError(call: TVOCall, error: NSError) {
+    console.log('callDidDisconnectWithError');
+    this.listener.onDisconnected(call);
+  }
+
+  callDidFailToConnectWithError(call: TVOCall, error: NSError) {
+    console.log('callDidFailToConnectWithError');
+    this.listener.onConnectFailure(call, error);
+  }
+}
+
+export class Twilio extends common.Common {
 
   public makeCall(phoneNumber, callListener, options: any = {}): any {
-    // TODO
+    const callDelegate = CallDelegate.initWithListener(callListener);
+
+    options.To = phoneNumber;
+    const params = NSDictionary.dictionaryWithDictionary(options);
+
+    return TwilioVoice.callParamsDelegate(this.accessToken, params, callDelegate);
   }
 }

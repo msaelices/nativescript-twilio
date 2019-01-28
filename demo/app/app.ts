62cfc6dbd90e812760fe185320996f2a332415e7
Manuel Saelices <msaelices@gmail.com>
2019-01-28 12:04:27 +0100

    Use the TwilioAppDelegate in the demo app.

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,3 +1,10 @@
-﻿import "./bundle-config";
+﻿import './bundle-config';
 import * as application from 'tns-core-modules/application';
-application.start({ moduleName: "main-page" });
+import { TwilioAppDelegate } from 'nativescript-twilio/delegate';
+
+if (application.ios) {
+  // register twilio app delegate in order to receive push notifications
+  application.ios.delegate = TwilioAppDelegate;
+}
+
+application.start({ moduleName: 'main-page' });
